4206ff8e5d1e62b519bcea6ec57aec039e53a734
alfonsom28 <alfonsomejias97@gmail.com>
2019-07-29 17:38:31 -0400

    Dashboard

---
+++ views/login.js
@@ -0,0 +1,31 @@
+function $(id) {
+	return document.getElementById(id);
+}
+
+function logIn(){
+    let body = {
+        username: document.getElementById("username").value,
+       contraseña: document.getElementById("password").value
+    };
+   console.log(body)
+    fetch("http://localhost:3000/login", {method: "POST", body:JSON.stringify(body)})
+        .then(function(resp){
+            return resp.json();
+        })
+        .then(function(data){
+            console.log(data);
+            if(data.status != undefined && data.status != null && data.status == 500){
+                alert(data.message);
+            }else if(data.redirect != null && data.redirect != undefined){
+                sessionStorage.setItem("name",body.username);
+             window.location.href = data.redirect;
+          }else if(data.message.includes("Banned") && data.status == 200){
+              alert(data.message);
+          }
+        });
+ }
+ function empty(){
+    document.getElementById("start").disabled = !document.getElementById("username").value.length;
+    document.getElementById("start").disabled = !document.getElementById("password").value.length;
+}
+ $("start").addEventListener("click", logIn);
---
+++ views/register.js
@@ -0,0 +1,35 @@
+function $(id) {
+	return document.getElementById(id);
+}
+function createAccount(){
+	var data = {
+			username: document.getElementById('username').value,
+			nombre: document.getElementById('name').value,
+            apellido: document.getElementById('lastname').value,
+            contraseña: document.getElementById('password').value,
+    }; 
+    console.log(data)  
+    let config = {
+		method: 'POST',
+		body: JSON.stringify(data),
+        };
+	fetch("http://localhost:3000/register", config)
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(data){
+			console.log(data);
+			alert(data.message);
+			if(data.redirect != null && data.redirect != undefined){
+            	window.location.href = data.redirect;
+         	}
+		});
+	}
+	function empty(){
+		document.getElementById("start2").disabled = !document.getElementById("username").value.length;
+		document.getElementById("start2").disabled = !document.getElementById("name").value.length;
+		document.getElementById("start2").disabled = !document.getElementById("lastname").value.length;
+		document.getElementById("start2").disabled = !document.getElementById("password").value.length;
+	}	
+
+document.getElementById("start2").addEventListener("click", createAccount);
